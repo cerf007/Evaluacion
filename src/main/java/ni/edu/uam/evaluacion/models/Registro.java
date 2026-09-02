abefcb4d1584019ba470c02438723c8d75ee85cf
cerf007 <cerf02062007@gmail.com>
2026-09-02 14:08:22 -0600

    Subiendo model preliminar

---
+++ src/main/java/ni/edu/uam/evaluacion/models/Registro.java
@@ -0,0 +1,20 @@
+package ni.edu.uam.evaluacion.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Registro {
+    private String nombres;
+    private String apellidos;
+    private String usuario;
+    private LocalDate fechaContratacion;
+    private Boolean tipoContrato;
+    private Boolean beneficios;
+    private String pathImagen;
+}
