4ab40bdec0cefc048de7b314d94a0ca19999ffd3
LitSoloSteven <stevencuadradcp@uamv.edu.ni>
2026-09-02 14:12:19 -0600

    Creacion del view

---
+++ src/main/java/ni/edu/uam/evaluacion/utils/AlertaUtil.java
@@ -0,0 +1,21 @@
+package ni.edu.uam.evaluacion.utils;
+
+import javafx.scene.control.Alert;
+
+public class AlertaUtil {
+    public static void mostrarError(String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error de Validación");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    public static void mostrarInfo(String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Información");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+}
